ed22b9d945dcb0c69c19b0594f8964f2c6e6c6da
Jason Li <jammylibro0815@gmail.com>
2025-07-26 00:29:15 -0400

    Update weaponChoiceMHWilds.py

--- weaponChoiceMHWilds.py
+++ weaponChoiceMHWilds.py
@@ -18,38 +18,60 @@ Bow = "Bow"
 weapons = [LS, GS, SnS, DB, Ham, HH, Lan, GL, SX, CB, IG, LB, HB, Bow]
 
 # ask the user about their playstyle
-print("Pick the 3 different playstyles:")
-print("1: High damage, but low speed")
-print("2: Low damage, but high speed")
-print("3: Support")
-play = int(input("So, what is your playstyle? "))
+def choice1():
+    print("Pick the 3 different playstyles:")
+    print("1: High damage, but low speed")
+    print("2: Low damage, but high speed")
+    print("3: Support")
+    play1 = int(input("So, what is your playstyle? "))
+    return play1
 
-# creates condition if fails to use a number
-if play < 3 or play > 1:
-    if play == 1:
-        weapons.remove(LS)
-        weapons.remove(DB)
-        weapons.remove(HH)
-        weapons.remove(LB)
-        weapons.remove(Bow)
-        print(weapons)
-    elif play == 2:
-        weapons.remove(GS)
-        weapons.remove(Ham)
-        weapons.remove(HH)
-        weapons.remove(Lan)
-        weapons.remove(HB)
-        print(weapons)
-    elif play == 3:
-        weapons.remove(LS)
-        weapons.remove(GS)
-        weapons.remove(SnS)
-        weapons.remove(DB)
-        weapons.remove(Ham)
-        weapons.remove(Lan)
-        weapons.remove(GL)
-        weapons.remove(SX)
-        weapons.remove(CB)
-        weapons.remove(IG)
-        weapons.remove(Bow)
-        print(weapons)
+# ask the user about their
+def choice2():
+    print("Pick the 3 types of damage:")
+    print("1: Cut Damage")
+    print("2: Blunt Damage")
+    print("3: Ammo Damage")
+    play2 = int(input("So, what kind of damage do you like? "))
+    return play2
+
+# creates condition for the choices about playstyle
+def play1(weapons):
+    play1 = choice1()
+    if play1 <= 3 or play1 >= 1:
+        if play1 == 1:
+            weapons.remove(LS)
+            weapons.remove(DB)
+            weapons.remove(HH)
+            weapons.remove(LB)
+            weapons.remove(Bow)
+        elif play1 == 2:
+            weapons.remove(GS)
+            weapons.remove(Ham)
+            weapons.remove(HH)
+            weapons.remove(Lan)
+            weapons.remove(HB)
+        elif play1 == 3:
+            weapons.remove(LS)
+            weapons.remove(GS)
+            weapons.remove(SnS)
+            weapons.remove(DB)
+            weapons.remove(Ham)
+            weapons.remove(Lan)
+            weapons.remove(GL)
+            weapons.remove(SX)
+            weapons.remove(CB)
+            weapons.remove(IG)
+            weapons.remove(Bow)
+    print(weapons)
+    return(weapons)
+
+def play2(weapons):
+    play2 = choice2()
+    if play1 <= 3 or play1 >= 1:
+        if play1 == 1:
+            if(any(weapons) == Ham):
+                weapons.remove(Ham)
+
+play1(weapons)
+play2(weapons)
